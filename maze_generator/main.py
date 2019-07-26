7bdaf284d009be9c4ffecf9fb6f5c05cc604a462
Laurent Noudohounsi <laurent.noudohounsi@mybraintech.com>
2019-07-26 12:15:51 +0200

    Init project

---
+++ maze_generator/main.py
@@ -0,0 +1,14 @@
+class MainMazeGenerator:
+
+    # --------------------------------------------------------------------------
+    # Initializatio
+    # --------------------------------------------------------------------------
+
+    def __init__(self):
+        print("Todo")
+
+    def main(self):
+        print("Todo")
+
+    if __name__ == '__main__':
+        MainMazeGenerator()
