23c9200fe96cd25ce749bbfce745ec5869eff105
Mathilde RESSIER <mathilde.ressier@mybraintech.com>
2019-07-26 14:38:49 +0200

    Create grid on Maze.py

--- maze_generator/maze_generator/Maze/maze.py
+++ maze_generator/maze_generator/Maze/maze.py
@@ -1,9 +1,48 @@
 from maze_generator.Maze.cell import Cell
 from maze_generator.Maze.coordinate import Coordinate
 from maze_generator.Maze.direction_type import DirectionType
+from numpy import matrix
 
+
+# ------------------------------------------------------------------------------
+# Representation of a maze grid with cells and walls
+# ------------------------------------------------------------------------------
 
 class Maze:
 
-    def __init__(self):
+    # --------------------------------------------------------------------------
+    # Properties
+    # --------------------------------------------------------------------------
+
+    @property
+    def width(self) -> int:
+        return self._width
+
+    @property
+    def height(self) -> int:
+        return self._height
+
+    # --------------------------------------------------------------------------
+    # Initialize
+    # --------------------------------------------------------------------------
+
+    def __init__(self, width: int, height: int):
+        self._width = width
+        self._height = height
+        self._grid = self.create_maze_grid()
+
+    # --------------------------------------------------------------------------
+    # Create the grid for the futur maze
+    # --------------------------------------------------------------------------
+
+    def create_maze_grid(self) -> [[Cell]]:
+        maze = []
+        for y in range(self._height):
+            maze_line = []
+            for x in range(self._width):
+                maze_line.append(Cell(Coordinate(x, y)))
+            maze.append(maze_line)
+        return maze
+
+    def open_wall(self, cell: Cell, direction: DirectionType):
         print("hello")
