a9b2378f109c40ee357eda0693d96cf56c9cecec
Laurent Noudohounsi <laurent.noudohounsi@mybraintech.com>
2019-07-26 14:36:55 +0200

    Update cell

--- maze_generator/maze_generator/Maze/cell.py
+++ maze_generator/maze_generator/Maze/cell.py
@@ -1,5 +1,6 @@
 from maze_generator.Maze.direction_type import DirectionType
 from maze_generator.Maze.coordinate import Coordinate
+import numpy as np
 
 
 class Cell:
@@ -12,24 +13,38 @@ class Cell:
     # --------------------------------------------------------------------------
 
     @property
-    def connections(self) -> [DirectionType, int]:
-        return self._connections
-
-    @property
     def is_visited(self) -> bool:
         return self._isVisited
+
+    @gain.isVisited
+    def is_visited(self, is_visited: bool):
+        self._isVisited = is_visited
 
     @property
     def coordinate(self) -> Coordinate:
         return self._coordinate
+
+    @property
+    def is_closed(self) -> bool:
+        return all(self._walls.values())
 
     # --------------------------------------------------------------------------
     # Initialization
     # --------------------------------------------------------------------------
 
     def __init__(self, coordinate: Coordinate):
-        self._connections = {
-            DirectionType.SOUTH: 1
-        }
         self._isVisited = False
         self._coordinate = coordinate
+        self._walls = {
+            DirectionType.NORTH: True,
+            DirectionType.EAST: True,
+            DirectionType.SOUTH: True,
+            DirectionType.WEST: True
+        }
+
+    # --------------------------------------------------------------------------
+    # Walls
+    # --------------------------------------------------------------------------
+
+    def open_wall(self, direction: DirectionType):
+        self._walls[direction] = False
