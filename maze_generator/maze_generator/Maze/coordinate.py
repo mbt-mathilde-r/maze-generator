a5a7622d14cb4f7702121c03769dbeef3f216fab
Laurent Noudohounsi <laurent.noudohounsi@mybraintech.com>
2019-07-26 15:00:42 +0200

    Funky update

--- maze_generator/maze_generator/Maze/coordinate.py
+++ maze_generator/maze_generator/Maze/coordinate.py
@@ -5,29 +5,9 @@ class Coordinate:
     """
 
     # --------------------------------------------------------------------------
-    # Properties
-    # --------------------------------------------------------------------------
-
-    @property
-    def x(self) -> int:
-        return self._x
-
-    @x.setter
-    def x(self, new_x: int):
-        self._x = new_x
-
-    @property
-    def y(self) -> int:
-        return self._y
-
-    @x.setter
-    def y(self, new_y: int):
-        self._y = new_y
-
-    # --------------------------------------------------------------------------
     # Initialization
     # --------------------------------------------------------------------------
 
     def __init__(self, x: int, y: int):
-        self._x = x
-        self._y = y
+        self.x = x
+        self.y = y
