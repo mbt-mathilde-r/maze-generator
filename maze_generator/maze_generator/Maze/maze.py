b81bbbc657ad5c8fc095fc6261e8510779dcc1fd
Mathilde RESSIER <mathilde.ressier@mybraintech.com>
2019-07-26 14:08:46 +0200

    Add Maze.py

---
+++ maze_generator/maze_generator/Maze/maze.py
@@ -0,0 +1,9 @@
+from maze_generator.Maze.cell import Cell
+from maze_generator.Maze.coordinate import Coordinate
+from maze_generator.Maze.direction_type import DirectionType
+
+
+class Maze:
+
+    def __init__(self):
+        print("hello")
